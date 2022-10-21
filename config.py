397cb3d62f8e1caf8ecf495875f7feb4c2cb5878
Dewang Agarwal <dk@lawn-128-61-20-245.lawn.gatech.edu>
2022-10-21 02:42:13 -0400

    CORS url updated

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[frontend_url],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
