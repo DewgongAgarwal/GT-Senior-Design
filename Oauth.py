fdaaf4329e157f009e35dff7ca47171e86424514
Dewang Agarwal <dk@lawn-128-61-20-245.lawn.gatech.edu>
2022-10-21 01:45:17 -0400

    Cookies

--- Oauth.py
+++ Oauth.py
@@ -27,7 +27,7 @@ def check_token(auth_token):
 
 def _add_cookie_to_reponse(response, params):
     for i in params:
-        response.set_cookie(key=i, value=params[i])
+        response.set_cookie(key=i, value=params[i],domain=frontend_url)
 
 
 def auth_token_generator(payload=None):
