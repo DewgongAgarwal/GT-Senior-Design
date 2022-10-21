697cbf3ca9c56d21b84c6d46d244de02cea723cd
Dewang Agarwal <dk@lawn-128-61-20-245.lawn.gatech.edu>
2022-10-21 04:43:31 -0400

    Updated domains

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ from decouple import config
 from fastapi.security import OAuth2PasswordBearer
 from datetime import datetime, timedelta, timezone
 
-frontend_url = "http://fe.mental-health-sd.com"
+frontend_url = "https://fe.mental-health-sd.com"
 
 key = config("KEY", default="")
 
